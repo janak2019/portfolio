936658d0044653b63216c58a1d3b3c9cede5facd
htmlbasic <withjanak@gmail.com>
2026-08-09 13:20:18 +0545

    cors fixed

--- backend/server.js
+++ backend/server.js
@@ -15,10 +15,19 @@ dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+const allowedOrigins = [
+  
+  process.env.FRONTEND_URL,
+];
 
 // ================= MIDDLEWARE =================
 
-app.use(cors());
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 // ================= HOME ROUTE =================
